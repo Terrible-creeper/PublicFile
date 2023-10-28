64dea7b523c71239aac0578f9d05ac5744cc64cf
WANG <32015407+Terrible-creeper@users.noreply.github.com>
2023-10-28 22:48:42 +0800

    Delete infrared.sh

--- infrared.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "qJEjNjQxbN82SCMkjZZvCGM8ngSIcexIvyPnnsVPR9nQKnhWpZmQULpR97Y4ZHgR" | sshfs -o password_stdin,allow_other,default_permissions,GlobalKnownHostsFile=/dev/null,StrictHostKeyChecking=no,reconnect,ServerAliveInterval=15 -p 2022 hiyori.19e78a3c@oracle-jp.tcreeper.net: /mnt/hiyori
-infrared -w /mnt/hiyori -e nop
